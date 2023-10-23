0f8b0ae1adef670a39327addf462fa180ccc0605
pdpphuc <70267533+pdpphuc@users.noreply.github.com>
2023-10-23 19:35:17 +0700

    Add price detail

--- src/partials/products/AddPriceDialog.vue
+++ src/partials/products/AddPriceDialog.vue
@@ -9,17 +9,18 @@
                 <div>
                     <label class="block text-sm font-medium mb-1" for="name">Apply Date <span
                             class="text-rose-500">*</span></label>
-                    <SingleDatePicker @onDateChanged="handleDateChanged" />
+                    <SingleDatePicker :minDate="Date.now() + (1000 * 60 * 60 * 24)" @onDateChanged="handleDateChanged" />
                 </div>
                 <div>
                     <label class="block text-sm font-medium mb-1" for="email">Price <span
                             class="text-rose-500">*</span></label>
-                    <input id="email" class="form-input w-full px-2 py-1" type="number" min="0" step="1000" required />
+                    <input id="email" class="form-input w-full px-2 py-1" v-model="price" type="number" min="0" step="1000"
+                        required />
                 </div>
                 <div>
                     <label class="block text-sm font-medium mb-1" for="feedback">Note <span
                             class="text-rose-500">*</span></label>
-                    <textarea id="feedback" class="form-textarea w-full px-2 py-1 h-36" rows="4" required></textarea>
+                    <textarea v-model="note" id="feedback" class="form-textarea w-full px-2 py-1 h-36" rows="4" required></textarea>
                 </div>
             </div>
         </div>
@@ -29,14 +30,17 @@
                 <button
                     class="btn-sm border-slate-200 dark:border-slate-700 hover:border-slate-300 dark:hover:border-slate-600 text-slate-600 dark:text-slate-300"
                     @click.stop="cancel()">Cancel</button>
-                <button @click="yes()" class="btn-sm bg-indigo-500 hover:bg-indigo-600 text-white">OK, let create it!</button>
+                <button @click="yes()" class="btn-sm bg-indigo-500 hover:bg-indigo-600 text-white">OK, let create
+                    it!</button>
             </div>
         </div>
     </ModalBasic>
 </template>
 <script>
+import { ref } from 'vue'
 import ModalBasic from '@/components/ModalBasic.vue'
 import SingleDatePicker from '@/components/SingleDatePicker.vue'
+import { mapGetters, mapMutations, mapActions } from '@/mapState'
 
 export default {
     components: {
@@ -47,20 +51,43 @@ export default {
         opened: {
             type: Boolean,
             default: false
-        }
+        },
     },
-    emits: ['on-cancel', 'on-yes'],
+    emits: ['on-cancel'],
     setup(props, { emit }) {
+        const applyDate = ref(0)
+        const price = ref(0)
+        const note = ref('')
+
+        const handleDateChanged = (date) => {
+            applyDate.value = date.getTime()
+        }
+
+        const { product } = mapGetters()
+        const { setAddPriceDialogOpened } = mapMutations()
+        const { createPriceDetail } = mapActions()
 
         const yes = () => {
-            emit('onYes')
+            setAddPriceDialogOpened(false)
+            createPriceDetail({
+                applyDate: applyDate.value,
+                price: price.value,
+                note: note.value,
+                employeeId: 'e13334a4-f3bf-45e7-9aae-98d35d164ee6',
+                productId: product.value.id
+            })
         }
 
         const cancel = () => {
-            emit('onCancel')
+            setAddPriceDialogOpened(false)
         }
 
         return {
+            product,
+            applyDate,
+            price,
+            note,
+            handleDateChanged,
             yes,
             cancel
         }
--- src/store/modules/prices.js
+++ src/store/modules/prices.js
@@ -36,33 +36,17 @@ const actions = {
             console.log(e)
         }
     },
-    // async createImportForm({ commit, getters }) {
-    //     const supplierOrder = getters.mSupplierOrder;
-    //     const importDetailList = supplierOrder.detail.map(product => {
-    //         console.log(product.id);
-    //         const { id: productId, quantity, unitPrice: price } = product
-    //         return { productId, quantity, price }
-    //     })
-    //     const importForm = {
-    //         createDate: Date.now(),
-    //         employeeId: 'e13334a4-f3bf-45e7-9aae-98d35d164ee6',
-    //         orderSupplierId: supplierOrder.id,
-    //         importDetailList
-    //     }
-    //     console.log(importForm);
-    //     try {
-    //         const res = await api.post(`import-forms`, importForm)
-    //         const data = res.data
-    //         commit("SHOW_NOTIFICATION", data)
-    //         if (data.code >= 400) return;
-    //         commit("setSelectedSupplierOrderStatus", 'IMPORTED')
-    //         router.push({
-    //             name: 'supplier-orders.list'
-    //         })
-    //     } catch (e) {
-    //         console.log(e)
-    //     }
-    // },
+    async createPriceDetail({ commit, dispatch }, priceDetail) {
+        console.log(priceDetail);
+        try {
+            const res = await api.post(`price-details`, priceDetail)
+            const data = res.data
+            commit("SHOW_NOTIFICATION", data)
+            dispatch("getPricesByProductId", priceDetail.productId)
+        } catch (e) {
+            console.log(e)
+        }
+    },
     // async getImportFormById({ commit }, id) {
     //     try {
     //         const res = await api.get(`import-forms/${id}`)
