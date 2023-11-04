2606fe36ad4b7e8d4f8f3e5ee090bad2e8e721ab
pdpphuc <70267533+pdpphuc@users.noreply.github.com>
2023-11-05 00:42:54 +0700

    Update ProductSearchModal.vue

--- src/partials/supplier_orders/ProductSearchModal.vue
+++ src/partials/supplier_orders/ProductSearchModal.vue
@@ -1,53 +1,70 @@
 <template>
   <!-- Modal backdrop -->
-  <transition
-    enter-active-class="transition ease-out duration-200"
-    enter-from-class="opacity-0"
-    enter-to-class="opacity-100"
-    leave-active-class="transition ease-out duration-100"
-    leave-from-class="opacity-100"
-    leave-to-class="opacity-0"
-  >
-    <div v-show="modalOpen" class="fixed inset-0 bg-slate-900 bg-opacity-30 z-50 transition-opacity" aria-hidden="true"></div>
+  <transition enter-active-class="transition ease-out duration-200" enter-from-class="opacity-0"
+    enter-to-class="opacity-100" leave-active-class="transition ease-out duration-100" leave-from-class="opacity-100"
+    leave-to-class="opacity-0">
+    <div v-show="modalOpen" class="fixed inset-0 bg-slate-900 bg-opacity-30 z-50 transition-opacity" aria-hidden="true">
+    </div>
   </transition>
   <!-- Modal dialog -->
-  <transition
-    enter-active-class="transition ease-in-out duration-200"
-    enter-from-class="opacity-0 translate-y-4"
-    enter-to-class="opacity-100 translate-y-0"
-    leave-active-class="transition ease-in-out duration-200"
-    leave-from-class="opacity-100 translate-y-0"
-    leave-to-class="opacity-0 translate-y-4"
-  >
-    <div v-show="modalOpen" :id="id" class="fixed inset-0 z-50 overflow-hidden flex items-start top-20 mb-4 justify-center px-4 sm:px-6" role="dialog" aria-modal="true">
-      <div ref="modalContent" class="bg-white dark:bg-slate-800 border border-transparent dark:border-slate-700 overflow-auto max-w-2xl w-full max-h-full rounded shadow-lg">
+  <transition enter-active-class="transition ease-in-out duration-200" enter-from-class="opacity-0 translate-y-4"
+    enter-to-class="opacity-100 translate-y-0" leave-active-class="transition ease-in-out duration-200"
+    leave-from-class="opacity-100 translate-y-0" leave-to-class="opacity-0 translate-y-4">
+    <div v-show="modalOpen" :id="id"
+      class="fixed inset-0 z-50 overflow-hidden flex items-start top-16 mb-10 justify-center px-4 sm:px-6" role="dialog"
+      aria-modal="true">
+      <div ref="modalContent"
+        class="bg-white dark:bg-slate-800 border border-transparent dark:border-slate-700 overflow-auto w-full max-w-6xl max-h-full rounded shadow-lg">
         <!-- Search form -->
-        <form class="border-b border-slate-200 dark:border-slate-700">
+        <div class="w-[71rem] fixed border-b border-slate-200 dark:border-slate-700">
           <div class="relative">
             <label :for="searchId" class="sr-only">Search</label>
-            <input :id="searchId" class="w-full dark:text-slate-300 bg-white dark:bg-slate-800 border-0 focus:ring-transparent placeholder-slate-400 dark:placeholder-slate-500 appearance-none py-3 pl-10 pr-4" type="search" placeholder="Search by name…" ref="searchInput" />
+            <input :id="searchId" v-model="searchText" @keydown.native.enter="search()"
+              class="w-full dark:text-slate-300 bg-white dark:bg-slate-800 border-0 focus:ring-transparent placeholder-slate-400 dark:placeholder-slate-500 appearance-none py-3 pl-10 pr-4"
+              type="search" placeholder="Search by name or description…" ref="searchInput" />
             <button class="absolute inset-0 right-auto group" type="submit" aria-label="Search">
-              <svg class="w-4 h-4 shrink-0 fill-current text-slate-400 dark:text-slate-500 group-hover:text-slate-500 dark:group-hover:text-slate-400 ml-4 mr-2" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
-                <path d="M7 14c-3.86 0-7-3.14-7-7s3.14-7 7-7 7 3.14 7 7-3.14 7-7 7zM7 2C4.243 2 2 4.243 2 7s2.243 5 5 5 5-2.243 5-5-2.243-5-5-5z" />
-                <path d="M15.707 14.293L13.314 11.9a8.019 8.019 0 01-1.414 1.414l2.393 2.393a.997.997 0 001.414 0 .999.999 0 000-1.414z" />
+              <svg
+                class="w-4 h-4 shrink-0 fill-current text-slate-400 dark:text-slate-500 group-hover:text-slate-500 dark:group-hover:text-slate-400 ml-4 mr-2"
+                viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M7 14c-3.86 0-7-3.14-7-7s3.14-7 7-7 7 3.14 7 7-3.14 7-7 7zM7 2C4.243 2 2 4.243 2 7s2.243 5 5 5 5-2.243 5-5-2.243-5-5-5z" />
+                <path
+                  d="M15.707 14.293L13.314 11.9a8.019 8.019 0 01-1.414 1.414l2.393 2.393a.997.997 0 001.414 0 .999.999 0 000-1.414z" />
               </svg>
             </button>
           </div>
-        </form>
+        </div>
         <div class="py-4 px-2">
           <!-- Recent searches -->
-          <div class="mb-3 last:mb-0">
+          <div class="mb-3 last:mb-0 mt-8">
             <!-- <div class="text-xs font-semibold text-slate-400 dark:text-slate-500 uppercase px-2 mb-2">Recent searches</div> -->
-            <ul class="text-sm">
-              <li v-for="product in products">
-                <div class="flex items-center p-2 text-slate-800 dark:text-slate-100 hover:text-white hover:bg-indigo-500 rounded group" to="#0" @click="handleSelect(product)">
-                  <svg class="w-4 h-4 fill-current text-slate-400 dark:text-slate-500 group-hover:text-white group-hover:text-opacity-50 shrink-0 mr-3" viewBox="0 0 16 16">
-                    <path d="M15.707 14.293v.001a1 1 0 01-1.414 1.414L11.185 12.6A6.935 6.935 0 017 14a7.016 7.016 0 01-5.173-2.308l-1.537 1.3L0 8l4.873 1.12-1.521 1.285a4.971 4.971 0 008.59-2.835l1.979.454a6.971 6.971 0 01-1.321 3.157l3.107 3.112zM14 6L9.127 4.88l1.521-1.28a4.971 4.971 0 00-8.59 2.83L.084 5.976a6.977 6.977 0 0112.089-3.668l1.537-1.3L14 6z" />
-                  </svg>
-                  <span>{{ product.name }}</span>
-                </div>
-              </li>
-            </ul>
+            <table>
+              <thead>
+                <!-- <th class="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                  <div class="font-semibold text-left">Image</div>
+                </th>
+                <th class="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                  <div class="font-semibold text-left">Name</div>
+                </th>
+                <th class="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                  <div class="font-semibold text-left">Description</div>
+                </th> -->
+              </thead>
+              <tbody>
+                <tr class="hover:bg-lime-500 hover:text-white border-b border-slate-200 dark:border-slate-700"
+                  @click="handleSelect(product)" v-for="product in products">
+                  <td class="px-2 first:pl-5 last:pr-5 py-3 min-w-[60%]">
+                    <img :src="product.imageList[0]" />
+                  </td>
+                  <td class="px-2 first:pl-5 last:pr-5 py-3">
+                    <div>{{ product.name }}</div>
+                  </td>
+                  <td class="px-2 first:pl-5 last:pr-5 py-3">
+                    <div>{{ product.description }}</div>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
@@ -57,15 +74,23 @@
 
 <script>
 import { ref, nextTick, onMounted, onUnmounted, watch } from 'vue'
+import { mapActions, mapMutations, mapGetters } from '@/mapState'
 
 export default {
   props: ['id', 'searchId', 'modalOpen', 'products'],
   emits: ['open-modal', 'close-modal'],
+  watch: {
+    modalOpen(newValue) {
+      if (!newValue) {
+        this.searchText = ''
+      }
+    }
+  },
   setup(props, { emit }) {
 
     const modalContent = ref(null)
     const searchInput = ref(null)
-    
+
     // close on click outside
     const clickHandler = ({ target }) => {
       if (!props.modalOpen || modalContent.value.contains(target)) return
@@ -91,15 +116,24 @@ export default {
     watch(() => props.modalOpen, (open) => {
       open && nextTick(() => searchInput.value.focus())
     })
-    
+
     const handleSelect = (product) => {
       emit('close-modal', product)
+    }
+
+    const { getPProducts } = mapActions()
+
+    const searchText = ref('')
+    const search = () => {
+      getPProducts({ categoryId: '', page: 1, keyWord: searchText.value })
     }
 
     return {
       modalContent,
       searchInput,
-      handleSelect
+      handleSelect,
+      searchText,
+      search
     }
   }
 }
