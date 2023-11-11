ac0d5b0fb34eab563383c786777857595a970ff0
pdpphuc <70267533+pdpphuc@users.noreply.github.com>
2023-11-11 13:21:59 +0700

    Not allow editing myself

--- src/partials/employees/EmployeesTableItem.vue
+++ src/partials/employees/EmployeesTableItem.vue
@@ -25,7 +25,7 @@
     </td>
     <td class="px-2 first:pl-5 last:pr-5 py-3 w-px">
       <div class="space-x-1">
-        <button @click="onEdit(employee.id)" class="text-slate-400 hover:text-slate-500 dark:text-slate-500 dark:hover:text-slate-400 rounded-full">
+        <button v-if="employee.id !== localStorageHelper.getUser()?.id" @click="onEdit(employee.id)" class="text-slate-400 hover:text-slate-500 dark:text-slate-500 dark:hover:text-slate-400 rounded-full">
           <span class="sr-only">Edit</span>
           <svg class="w-8 h-8 fill-current" viewBox="0 0 32 32">
               <path d="M19.7 8.3c-.4-.4-1-.4-1.4 0l-10 10c-.2.2-.3.4-.3.7v4c0 .6.4 1 1 1h4c.3 0 .5-.1.7-.3l10-10c.4-.4.4-1 0-1.4l-4-4zM12.6 22H10v-2.6l6-6 2.6 2.6-6 6zm7.4-7.4L17.4 12l1.6-1.6 2.6 2.6-1.6 1.6z" />
@@ -46,6 +46,7 @@
 <script>
 import { computed } from 'vue'
 import { mapActions } from '@/mapState'
+import localStorageHelper from '@/helpers/localStorageHelper'
 
 export default {
   props: ['employee', 'value', 'selected'],
@@ -105,6 +106,7 @@ export default {
       }
     }    
 
+    const { user } = mapActions()
     const { getEmployeeById } = mapActions()
 
     const onEdit = (empId) => {
@@ -122,7 +124,8 @@ export default {
       statusColor,
       typeIcon,
       onEdit,
-      onDelete
+      onDelete,
+      localStorageHelper
     }
   },
 }
